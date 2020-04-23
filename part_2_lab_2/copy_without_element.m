afb8ea2ab63812c90c8f5e916dce9df5493bec19
Piotr Chorościn <titkot96@gmail.com>
2020-04-23 19:41:27 +0200

    Add own implementation but not working

---
+++ part_2_lab_2/copy_without_element.m
@@ -0,0 +1,3 @@
+function [A_without_n] = copy_without_element(A, n)
+    A_without_n = A([1:(n-1) (n+1):end]);
+end
--- part_2_lab_2/exercice2.m
+++ part_2_lab_2/exercice2.m
@@ -8,32 +8,56 @@ U{3} = max(0, rand(I(3), J));
 %Generowanie syntetycznych obserwacji
 %ones(J, 1) - wektor kolumnowy wag (wszystkie takie same i równe 1)
 Y=ktensor(ones(J, 1), U);
+U_est = CP(Y, 5, J, 10);
 
-function U_est = CP(Y, P, J, iterations)
+function U = CP(Y, P, J, iterations)
    % N - liczba modów
-   I = size(Y)
+   I = size(Y);
    
    % losowa inicjalizacja faktorów
-   U{1} = max(0, rand(I(1), J));
-   U{2} = max(0, rand(I(2), J));
-   U{3} = max(0, rand(I(3), J));
+   for i=1:size(I, 2)
+       U{i} = max(0, rand(I(i), J));
+   end
    
-   % przemiatanie po wszystkich (N) modach tensora obserwacji Y
    N = size(Y.u, 1); % liczba modów
    Y_arr = double(Y); % konwersja tensora na tablicę wielowymiarową
+   
+   % wyznaczanie faktorów powtórzone iterations razy
    for i = 1:iterations
+       
+       % przemiatanie po wszystkich (N) modach tensora obserwacji Y
        for n = 1:N
            % Matrycyzacja względem n-tego modu
            Yn = unfold(Y_arr, n);
            
            % Estymacja n-tego faktora za pomocą m. najmniejszych kwadratów
-           % Macież iloczynów Khatri-Rao pomiędzy faktorami (poza n-tym) 
+           % Wszystkie faktory poza n-tym
+           indexes_without_n = copy_without_element(1:N, n);
+           U_without_n = cell(1, N-1);
+           U_without_n_array_squared = zeros(N-1, J, J);
+           for j = 1:(N-1)
+               cell_index = indexes_without_n(j);  
+               U_without_n{j} = U{cell_index};
+               U_without_n_array_squared(j, :, :) = (U{cell_index})' * U{cell_index};
+           end
+           % Iloczyn Hadamarda między poszczególnymi Ui'*Ui
+           B = fold(@(ui, uj) ui * uj, U_without_n_array_squared);
            
-           % TODO: U_kr_n = fold(@kr, [U{n}, n]);
-           % TODO: U{n} = argmin(n) 0.5||Y-
+           % Wyznaczenie faktora U{n}
+           U{n} = (Yn*U_without_n) / B;
            
-           % TODO: Skalowanie n-tego faktora
+           if n ~= N
+              % Skalowanie n-tego faktora
+              
+           end
        end
    end
 end
+
+function [C] = hadamard_product(A, B)
+    C = (A' * A) .* (B' * B);
+end
+
+
+
 
