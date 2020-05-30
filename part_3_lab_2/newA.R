86dfffde993db5fd9bb98f4e5823119d3fdd1e6b
Dawid <dave201000@gmail.com>
2020-05-30 18:33:29 +0200

    task A almost finished

--- part_3_lab_2/newA.R
+++ part_3_lab_2/newA.R
@@ -1,17 +1,27 @@
-# Title     : Computional Intelligence
-# Objective : Examine Genetic Algorithm
-# Created by: Piotr
-# Created on: 20.05.2020
-
 require("GA")
 require("globalOptTests")
+
+# Operatory mutacji
+# omutation.custom = custom.mutation.function
+omutation.default = gaperm_simMutation
+omutation.range = c(gaperm_simMutation, gaperm_ismMutation,
+                    gaperm_swMutation, gaperm_dmMutation,
+                    gaperm_scrMutation)
+omutation.names = c("custom", "sim", "ism", "sw", "dm", "scr")
+
+# Operatory krzyzowania
+# ocrossover.custom = custom.crossing.function
+ocrossover.default = gaperm_oxCrossover
+ocrossover.range = c(gaperm_cxCrossover, gaperm_pmxCrossover,
+                     gaperm_oxCrossover, gaperm_pbxCrossover)
+ocrossover.names = c("custom", "cx", "pmx", "ox", "pbx")
+oselection.default = gaperm_lrSelection
 
 
 # returns objective function given by name
 objective.fun.of <- function(function.name) {
   # Get the length of the parameter vector expected by a given objective function.
   problem.dimen <- getProblemDimen(function.name)
-
   function(x1, x2) {
     x1x2 <- c(x1, x2)
     goTest(par = c(x1x2, rep(0, problem.dimen - length(x1x2))), fnName = function.name, checkDim = TRUE)
@@ -42,25 +52,32 @@ GA.run.iterations <- function(function.name, iterations.count, crossover.operato
 }
 
 # runs experiment for every param.name value in param.range
-GA.run.experiment <- function(function.name, iterations.count) {
-  param.best.scores <- NULL # accumulator for best mean results in generation
-  param.mean.scores <- NULL # accumulator for mean mean results in generation
+GA.run.experiment <- function(function.name, iterations.count, crossover.range, mutation.range) {
+  operator.best.scores <- NULL # accumulator for best mean results in generation
+  operator.mean.scores <- NULL # accumulator for mean mean results in generation
   # GA.best <- NULL # best result
-    param.score <- GA.run.iterations(function.name, iterations.count)
-  list(best.scores = param.score$best.mean, mean.scores = param.score$mean.mean)
+  index <- 1
+  for(operator in crossover.range) {
+    operator.score <- GA.run.iterations(function.name, iterations.count, crossover.operator = operator)
+    operator.best.scores[index] <- operator.score$best.mean
+    operator.mean.scores[index] <- operator.score$mean.mean
+    index = index + 1
+  }
+  # print(operator.best.scores)
+  list(best.scores = operator.best.scores, mean.scores = operator.mean.scores)
 }
 
 # runs experiments as a batch
-GA.run.experiment.list <- function(function.name, iterations.count = 10) {
-    param.score.range <- GA.run.experiment(function.name, iterations.count)
-  print(param.score.range$mean.scores)
-    scores.plot(
-      x = 1,
-      y1 = param.score.range$best.scores,
-      y2 = param.score.range$mean.scores,
-      const = getGlobalOpt(function.name),
-      x.label = "scorez"
-    )
+GA.run.experiment.list <- function(function.name, iterations.count = 10, crossover.range, mutation.range) {
+  param.score.range <- GA.run.experiment(function.name, iterations.count, crossover.range, mutation.range)
+  print(param.score.range)
+  scores.plot(
+    x = seq_along(param.score.range$best.scores),
+    y1 = param.score.range$best.scores,
+    y2 = param.score.range$mean.scores,
+    const = getGlobalOpt(function.name),
+    x.label = "scores"
+  )
 }
 
 #plots best (y1) and mean (y2) scores for each param value x
@@ -74,4 +91,4 @@ type.name = "permutation"
 # experiments for Schuber function
 function.name <- "Schubert"
 
-GA.run.experiment.list(function.name, 10)
+GA.run.experiment.list(function.name, 10, ocrossover.range)
